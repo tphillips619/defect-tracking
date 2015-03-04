1073c9d12938d4e103347da25059f2b8aeb58072
Devin <tritaniv@gmail.com>
2015-03-04 02:02:52 -0800

    Config Class and SQL Helper in prep for sql methods coming up.

---
+++ src/main/webapp/TzConfig.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Devin on 3/4/2015.
+ */
+public class TzConfig {
+
+    public static final String MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String MYSQL_DB = "jdbc:mysql://192.168.1.23:3306/MySQL" + "?user=trackapp" + "&password=89Q9vlCCFNjZ";
+}
---
+++ src/main/webapp/TzSQLHelper.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by Devin on 3/4/2015.
+ */
+public class TzSQLHelper {
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+
+
+}
